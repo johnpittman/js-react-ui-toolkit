3b0d771174fe99e376b0edbeb694b7be59267f0f
John Pittman <john.pittman@outdoorsy.co>
2021-04-06 00:54:29 -0700

    docs: stories

--- src/components/core/Tag/component.stories.tsx
+++ src/components/core/Tag/component.stories.tsx
@@ -4,7 +4,7 @@ import type { Story, Meta } from '@storybook/react';
 import Tag, { TagProps } from './component';
 
 export default {
-  title: 'Base/Tag',
+  title: 'core/Tag',
   component: Tag,
   argTypes: {}
 } as Meta;
