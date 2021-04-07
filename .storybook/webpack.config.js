3ad906e4b9ae8258906add5a9f0731283ca1751f
John Pittman <john.pittman@outdoorsy.co>
2021-04-06 20:59:46 -0700

    ci: include deps

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,45 +1,45 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-// const { ESBuildMinifyPlugin } = require('esbuild-loader');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
 
-// function useEsbuildMinify(config, options) {
-//   const terserIndex = config.optimization.minimizer.findIndex(
-//     (minimizer) => minimizer.constructor.name === 'TerserPlugin'
-//   );
-//   if (terserIndex > -1) {
-//     config.optimization.minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
-//   }
-// }
+function useEsbuildMinify(config, options) {
+  const terserIndex = config.optimization.minimizer.findIndex(
+    (minimizer) => minimizer.constructor.name === 'TerserPlugin'
+  );
+  if (terserIndex > -1) {
+    config.optimization.minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
+  }
+}
 
-// function useEsbuildLoader(config, options) {
-//   const tsLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.ts'));
+function useEsbuildLoader(config, options) {
+  const tsLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.ts'));
 
-//   if (tsLoader) {
-//     tsLoader.use.loader = 'esbuild-loader';
-//     tsLoader.use.options = { ...options, loader: 'ts' };
-//   }
+  if (tsLoader) {
+    tsLoader.use.loader = 'esbuild-loader';
+    tsLoader.use.options = { ...options, loader: 'ts' };
+  }
 
-//   const tsxLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.tsx'));
+  const tsxLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.tsx'));
 
-//   if (tsxLoader) {
-//     tsxLoader.use.loader = 'esbuild-loader';
-//     tsxLoader.use.options = { ...options, loader: 'tsx' };
-//   }
+  if (tsxLoader) {
+    tsxLoader.use.loader = 'esbuild-loader';
+    tsxLoader.use.options = { ...options, loader: 'tsx' };
+  }
 
-//   const jsLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.js'));
+  const jsLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.js'));
 
-//   if (jsLoader) {
-//     jsLoader.use.loader = 'esbuild-loader';
-//     jsLoader.use.options = { ...options, loader: 'js' };
-//   }
+  if (jsLoader) {
+    jsLoader.use.loader = 'esbuild-loader';
+    jsLoader.use.options = { ...options, loader: 'js' };
+  }
 
-//   const jsxLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.jsx'));
+  const jsxLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.jsx'));
 
-//   if (jsxLoader) {
-//     jsxLoader.use.loader = 'esbuild-loader';
-//     jsxLoader.use.options = { ...options, loader: 'jsx' };
-//   }
-// }
+  if (jsxLoader) {
+    jsxLoader.use.loader = 'esbuild-loader';
+    jsxLoader.use.options = { ...options, loader: 'jsx' };
+  }
+}
 
 module.exports = ({ config }) => {
   const rules = config.module.rules;
@@ -99,13 +99,13 @@ module.exports = ({ config }) => {
     })
   );
 
-  // useEsbuildMinify(config, {
-  //   target: 'es2015' // Syntax to compile to (see options below for possible values)
-  // });
+  useEsbuildMinify(config, {
+    target: 'es2015' // Syntax to compile to (see options below for possible values)
+  });
 
-  // useEsbuildLoader(config, {
-  //   target: 'es2015'
-  // });
+  useEsbuildLoader(config, {
+    target: 'es2015'
+  });
 
   return config;
 };
