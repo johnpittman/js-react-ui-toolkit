eac0679aa5af4e517aaadb13b512d81d8a3a6a2b
John Pittman <john.pittman@outdoorsy.co>
2021-04-06 20:45:06 -0700

    build: do not override storybook optimizer plugins

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,43 +1,13 @@
 const path = require('path');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
-// const { ESBuildMinifyPlugin } = require('esbuild-loader');
+const { ESBuildMinifyPlugin } = require('esbuild-loader');
 
 function useEsbuildMinify(config, options) {
-  // const terserIndex = config.optimization.minimizer.findIndex(
-  //   (minimizer) => minimizer.constructor.name === 'TerserPlugin'
-  // );
-  // if (terserIndex > -1) {
-  //   config.optimization.minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
-  // }
-}
-
-function useEsbuildLoader(config, options) {
-  const tsLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.ts'));
-
-  if (tsLoader) {
-    tsLoader.use.loader = 'esbuild-loader';
-    tsLoader.use.options = { ...options, loader: 'ts' };
-  }
-
-  const tsxLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.tsx'));
-
-  if (tsxLoader) {
-    tsxLoader.use.loader = 'esbuild-loader';
-    tsxLoader.use.options = { ...options, loader: 'tsx' };
-  }
-
-  const jsLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.js'));
-
-  if (jsLoader) {
-    jsLoader.use.loader = 'esbuild-loader';
-    jsLoader.use.options = { ...options, loader: 'js' };
-  }
-
-  const jsxLoader = config.module.rules.find((rule) => rule.test && rule.test.test('.jsx'));
-
-  if (jsxLoader) {
-    jsxLoader.use.loader = 'esbuild-loader';
-    jsxLoader.use.options = { ...options, loader: 'jsx' };
+  const terserIndex = config.optimization.minimizer.findIndex(
+    (minimizer) => minimizer.constructor.name === 'TerserPlugin'
+  );
+  if (terserIndex > -1) {
+    config.optimization.minimizer.splice(terserIndex, 1, new ESBuildMinifyPlugin(options));
   }
 }
 
@@ -101,10 +71,6 @@ module.exports = ({ config }) => {
 
   useEsbuildMinify(config, {
     target: 'es2015' // Syntax to compile to (see options below for possible values)
-  });
-
-  useEsbuildLoader(config, {
-    target: 'es2015'
   });
 
   return config;
