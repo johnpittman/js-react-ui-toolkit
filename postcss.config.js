35e431ba3e3ca5d7ea38cf5c741a67456b8f9c4c
John Pittman <john.pittman@outdoorsy.co>
2021-04-08 22:42:14 -0700

    build: postcss

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
 module.exports = {
   plugins: {
-    // PostCSS Preset Env includes autoprefixer and browsers option will be passed to it automatically.
-    'postcss-preset-env': {
-      stage: 2,
-      browsers: 'last 2 versions'
-      // importFrom: 'path/to/file.css'
-    },
     'postcss-import': {},
-    'postcss-nested': {},
-    'postcss-pxtorem': {
-      // rootValue: 16
-    }
+    'postcss-nested': {}
   }
 };
--- scripts/build.js
+++ scripts/build.js
@@ -1,14 +1,12 @@
 const gulp = require('gulp');
 const postcss = require('gulp-postcss');
-const postcssImport = require('postcss-import');
-const postcssNested = require('postcss-nested');
 
 const projectPath = `${__dirname}/..`;
 
 function generateCss() {
   return gulp
     .src(`${projectPath}/src/**/*.css`)
-    .pipe(postcss([postcssImport(), postcssNested()]))
+    .pipe(postcss())
     .pipe(gulp.dest(`${projectPath}/lib`));
 }
 
