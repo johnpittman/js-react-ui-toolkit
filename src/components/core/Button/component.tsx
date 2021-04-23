95afbf906cb247a9da0f3684da29652edb10537f
John Pittman <john.pittman@outdoorsy.co>
2021-04-23 13:36:33 -0700

    refactor: folder structure

--- src/components/core/Button/component.tsx
+++ src/components/core/Button/component.tsx
@@ -1,6 +1,6 @@
 import React, { ReactNode, useEffect, useState } from 'react';
 
-import LoadingIndicator from '>/components/atoms/LoadingIndicator';
+import LoadingIndicator from '>/components/loaders/LoadingIndicator';
 
 import styles from './style.module.css';
 
